af28d4cf1ef2f90f5a034a4e9bbc6f1a74abf254
Erick-Torres-Dev <erickhenriquesantostorres@gmail.com>
2025-07-02 19:28:39 -0300

    Criação da pagina inicial

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,8 @@
 export default function Home() {
   return (
     <>
+    <h1>Prova Versionamento - RPV Senai <Cataguases></Cataguases></h1>
+    <p>Cataguases, 02 de Julho de 2025</p>
     </>
   )
 }
